9aedf08797d223d7baa43f72103be537612d2bc8
MyLindenius <my.lindenius@gmail.com>
2024-10-02 13:14:46 +0200

    H1 bank done

--- higher-grade/src/bank.c
+++ higher-grade/src/bank.c
@@ -24,11 +24,15 @@ account_t *account_new(unsigned int balance) {
   account_t *account = malloc(sizeof(account_t));
 
   account->balance = balance;
+  pthread_mutex_init(&account->lock, NULL); // Create the mutex lock for the account
 
   return account;
 }
 
-void account_destroy(account_t *account) {}
+void account_destroy(account_t *account) {
+  pthread_mutex_destroy(&account->lock); // Destory the mutex lock for the account
+  free(account); // Delete the account
+}
 
 /**
  * A purposefully stupid way to add two numbers that makes data
@@ -55,6 +59,7 @@ int sub(int a, int b) {
 }
 
 int transfer(int amount, account_t *from, account_t *to) {
+    pthread_mutex_lock(&from->lock); // Lock who we want to send from, so that the amount (that we check in if) doesnt get changed while we enter the if loop.
     if (from->balance >= amount) {
     from->balance = sub(from->balance, amount);
 
@@ -63,11 +68,17 @@ int transfer(int amount, account_t *from, account_t *to) {
      * randomized interleaving of the threads.
      */
     RANDOM_SLEEP();
+    pthread_mutex_lock(&to->lock); // Lock who we send to, so that the amount dont get changed somewhere else at the same time.
 
     to->balance = add(to->balance, amount);
+    
+    // Transfer is done, we can unlock the locks.
+    pthread_mutex_unlock(&to->lock);
+    pthread_mutex_unlock(&from->lock);
 
     return 0;
   } else {
+    pthread_mutex_unlock(&from->lock); // Since we cant enter the if, we need to unlock here to not cause deadlock
     return -1;
   }
 }
