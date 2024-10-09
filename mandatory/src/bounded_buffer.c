1bec84b71730469987ee1bae43833c61b4dda35e
darkfrobozz <darkfrobozz.mega@gmail.com>
2024-10-09 13:08:44 +0200

    bounded buffer

--- mandatory/src/bounded_buffer.c
+++ mandatory/src/bounded_buffer.c
@@ -43,6 +43,11 @@ void buffer_destroy(buffer_t *buffer) {
   buffer->mutex = NULL;
 
   // TODO: Deallocate any other resources allocated to the bounded buffer.
+  // Destroy the other semaphore for full and empty behavior
+  psem_destroy(buffer->data);
+  buffer->data = NULL;
+  psem_destroy(buffer->empty);
+  buffer->empty = NULL;
 
 }
 
@@ -71,24 +76,35 @@ void buffer_print(buffer_t *buffer) {
 
 void buffer_put(buffer_t *buffer, int a, int b) {
   // TODO: Add the needed synchronization.
+  psem_wait(buffer->empty);
+  psem_wait(buffer->mutex);
 
   // Insert the tuple (a, b) into the buffer. 
   buffer->array[buffer->in].a = a;
   buffer->array[buffer->in].b = b;
 
   // TODO: Update buffer->in and make sure it wraps around.
+  buffer->in = (buffer->in + 1)%buffer->size;
+
 
   // TODO: Add the needed synchronization.
+  psem_signal(buffer->mutex);
+  psem_signal(buffer->data);
 }
 
 void buffer_get(buffer_t *buffer, tuple_t *tuple) {
   // TODO: Add the needed synchronization.
+  psem_wait(buffer->data);
+  psem_wait(buffer->mutex);
 
   // Read the tuple (a, b) from the buffer.
   tuple->a = buffer->array[buffer->out].a;
   tuple->b = buffer->array[buffer->out].b;
 
   // TODO: Update buffer->out and make sure it wraps around.
+  buffer->out = (buffer->out + 1)%buffer->size;
 
   // TODO: Add the needed synchronization.
+  psem_signal(buffer->mutex);
+  psem_signal(buffer->empty);
 }
